93515ecafa2d8dc45d95fd6ecfc8d8bbedd16982
yuyasaito210 <yuyasaito210@gmail.com>
2018-05-10 20:33:18 -0700

    udpated backend

--- client/reducers/get_recipes.js
+++
@@ -1,9 +0,0 @@
-import { GET_RECIPES } from '../../actions/actions';
-
-export default function getRecipe(state = {}, action) {
-  switch (action.type) {
-    case GET_RECIPES:
-      return action.payload
-  }
-  return state;
-}
--- client/reducers/loading_recipe.js
+++
@@ -1,9 +0,0 @@
-import { LOADING_RECIPES } from '../../actions/actions';
-
-export default function loadingRecipes(state = true, action) {
-  switch (action.type) {
-    case LOADING_RECIPES:
-      return action.payload;
-  }
-  return state;
-}
